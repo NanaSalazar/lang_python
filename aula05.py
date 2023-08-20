749bbc47a188705cf9406d38e62c4609e551d1b0
Ana Salazar <102234649+NanaSalazar@users.noreply.github.com>
2023-08-20 10:11:35 -0300

    att

--- aula05.py
+++ aula05.py
@@ -15,17 +15,24 @@ class ContaCorrente(ContaBancaria):
         self.saldo = 0
         self.cartaoCredito = bool
     
-    def setSaldo(valor):
+    def deposito(valor):
         saldo =+valor
     
     def saque(valor, saldo):
         if (valor < saldo):
-            return print('Você não possui saldo suficiente para esta operação')
+            print('Você não possui saldo suficiente para a operacao. Deseja utilizar cheque especial? ')
+            autChequeEspecial = int(input())
+            if autChequeEspecial == 1:
+                chequeEspecial = valor - saldo
+            else:
+                print('Voce nao autorizou o cheque especial, portanto nao e possivel realizar o saque')
+            
+        
         else:
             print(f'Voce sacou {valor} da sua conta.')
             print(f'Seu saldo atual e {saldo}')
             return saldo - valor
-        
+    
 
 cliente = input('Digite seu nome: ')
 
@@ -40,13 +47,37 @@ tipoConta = int(input())
 if tipoConta == 1:
     newCliente = ContaCorrente(tipoConta, cliente, 0, bool)
     saldo = 0
-    cartaoCredito = print(input('Deseja cartao de credito?: '))
-    if cartaoCredito == 'sim':
-        cartaoCredito == True
-    elif cartaoCredito == 'nao':
-        cartaoCredito == False
+    print('Deseja operar sua conta?')
+    print('1. Sim')
+    print('2. Nao')
+    operar = int(input())
 
-print(newCliente.cartaoCredito)
-print(newCliente.saldo)        
+    if operar == 1:
+        print('Escolha a opcao desejada: ')
+        print('1. Cartao de Credito')
+        print('2. Deposito')
+        print('3. Saque')
+        print('4. Sair')
+        tipoOperacao = int(input())
+
+        while tipoOperacao != 4:
+            if tipoOperacao == 1:
+                input('Você deseja cartao de credito? ')
+                break
+            elif tipoOperacao == 2:
+                input('Você deseja depositar?')
+                break
+            elif tipoOperacao == 3:
+                input('Voce deseja sacar? ')
+                break
+            elif tipoOperacao == 4:
+                input('Voce finalizou suas operacoes')
+                break
+            else:
+                input('Operacao invalida')
+    
+    if operar == 2:
+        print('Obrigado por utilizar os servicos!')
+
 
         
